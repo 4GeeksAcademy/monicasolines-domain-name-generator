a87206e777f8bb42afe9a0e5af15c8d2b3f557af
Monica Solines <160787107+monicasolines@users.noreply.github.com>
2024-07-31 19:41:28 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,29 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronoun = ["the", "our", "your"];
+  let adj = ["great", "big", "best"];
+  let noun = ["programmer", "bot", "website"];
+  let domains = [".com", ".net", ".us"];
+
+  let randomDomain = [];
+  let domainHTML = ` `;
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < domains.length; l++) {
+          let domain = pronoun[i] + adj[j] + noun[k] + domains[l];
+          randomDomain.push(domain);
+
+          domainHTML += `
+          <li class="list-group-item">${domain}</li>
+          `;
+        }
+      }
+    }
+  }
+
+  document.querySelector(".generator").innerHTML = domainHTML;
 };
